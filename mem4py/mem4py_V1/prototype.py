347b4baaaac6f923323e35b1ad8b4045c39766c4
Jusyl236 <jul.chauvin@gmail.com>
2025-11-24 23:48:17 +0100

    licence

--- mem4py/mem4py_V1/prototype.py
+++ mem4py/mem4py_V1/prototype.py
@@ -1,4 +1,20 @@
 """
+Mem4Py v1 — Proto-laboratoire de gouvernance cognitive
+Copyright [2025] [Julien Chauvin - Cafe virtuel]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+"""
 Mem4Py - Prototype de Mem4ristor à 5 états.
 Créé lors de la Session 1 du Café Virtuel (19/08/2025).
 Implémentation mathématique par DeepSeek (20/08/2025).
